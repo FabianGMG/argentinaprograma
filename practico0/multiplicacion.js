49a184322806680991731f54e1bef87128f76314
unknown <fabiangmg@gmail.com>
2023-05-20 10:35:53 -0300

    Cargo resta y multiplicacion.

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,5 @@
+let x=2;
+let y=17;
+let z=6;
+console.log(y*x*z);
+//"El resultado de la multiplicación es 204.
---
+++ practico0/resta.js
@@ -0,0 +1,4 @@
+let x=10;
+let y=2;
+console.log(x-y);
+//"El resultado de la resta es 8.
