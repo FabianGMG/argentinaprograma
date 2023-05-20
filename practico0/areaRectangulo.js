9a4d333f8e8e982a0ff63ff9053467131636b232
unknown <fabiangmg@gmail.com>
2023-05-20 10:43:11 -0300

    Práctico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+let base=5;
+let altura=3;
+console.log(base*altura);
+let base2=12.5;
+let altura2=5.3;
+console.log(base2*altura2);
+//"Si la base es 5cm y la altura es 3cm, el área del rectángulo es 15cm cuadrados.
+//"Si la base es 12,5cm y la altura es 5,3cm, el área del rectángulo es 66,25cm cuadrados.
---
+++ practico0/promedio3.js
@@ -0,0 +1,16 @@
+let x1=5;
+let y1=7;
+let z1=9;
+console.log((x1+y1+z1)/3);
+let x2=-2;
+let y2=-3;
+let z2=-8;
+console.log((x2+y2+z2)/3);
+let x3=-1.2;
+let y3=5.3;
+let z3=-3.3
+console.log((x3+y3+z3)/3);
+//El primer promedio da 7.
+//El segundo promedio da -4,33.
+//El tercer promedio da 0,27.
+
